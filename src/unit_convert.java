98df8fdeff86c4f05e592aba3044a85dacca29d4
dennis <dennis.bernic@gmail.com>
2019-08-30 00:54:39 +0300

    alala

--- src/unit_convert.java
+++ src/unit_convert.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 //adaugat metric-converter
 public class unit_convert {
 	public static void main (String[] args) {
 		System.out.println("the conversion is being started");
 		System.out.println("hai sa comparam mere cu pere");
 		
+		Scanner user_int = new Scanner(System.in);
+		
+		int numar = user_int.nextInt();
+		System.out.print("provide your integer:"+);
 	}
 	
 

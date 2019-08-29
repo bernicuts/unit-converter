2096310a3f1d2167085ca4971627866ca2fa6c89
dennis <dennis.bernic@gmail.com>
2019-08-30 00:29:01 +0300

    zaibit

--- src/unit_convert.java
+++ src/unit_convert.java
@@ -1,4 +1,10 @@
 
 public class unit_convert {
+	public static void main (String[] args) {
+		System.out.println("the conversion is being started");
+		System.out.println("hai sa comparam mere cu pere");
+		
+	}
+	
 
 }

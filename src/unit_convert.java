e56327f565ce5a9591c46cabc00febca034a35ad
dennis <dennis.bernic@gmail.com>
2019-08-31 00:42:40 +0300

    first version challange complete

--- src/unit_convert.java
+++ src/unit_convert.java
@@ -5,19 +5,20 @@ public class unit_convert {
 	public static void main (String[] args) {
 		System.out.println("___the conversion is being started___");
 		System.out.println("-\\hai sa comparam mere cu pere\\-");
-
-		Scanner user_int = new Scanner(System.in);
 		
-		boolean varType = true ;
+		boolean varType = true ; // that would be a sentinel variable
+		
 		while(varType) {
+			
 			try {
+				Scanner user_int = new Scanner(System.in);
 				System.out.print("provide your integer:");
 				int numar = user_int.nextInt();
-				System.out.print("you have entered:" + numar);
+				System.out.print("you have successfuly entered:" + numar);
 				varType = false;
 			}
 			catch (Exception e) {
-				System.out.print("value invalid");
+				System.out.println("value invalid, try again with a number...\n");
 			}
 		}
 

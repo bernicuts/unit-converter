23146449f0d5a5d98707f1a603213009cd8659c7
dennis <dennis.bernic@gmail.com>
2019-08-30 19:02:11 +0300

    pun

--- src/unit_convert.java
+++ src/unit_convert.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 //adaugat metric-converter
 public class unit_convert {
 	public static void main (String[] args) {
-		System.out.println("the conversion is being started");
-		System.out.println("hai sa comparam mere cu pere");
+		System.out.println("___the conversion is being started___");
+		System.out.println("-\\hai sa comparam mere cu pere\\-");
 		
 		Scanner user_int = new Scanner(System.in);
-		System.out.println("provide your integer");
+		System.out.print("provide your integer:");
 		int numar = user_int.nextInt();
-
+		System.out.print("you have entered:" + numar);
+	
 	}
 
 }

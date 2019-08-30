22cf9f01f4f9eaa090435c9dc88fea4c46c7feb7
dennis <dennis.bernic@gmail.com>
2019-08-30 19:16:45 +0300

    try try

--- src/unit_convert.java
+++ src/unit_convert.java
@@ -5,10 +5,17 @@ public class unit_convert {
 	public static void main (String[] args) {
 		System.out.println("___the conversion is being started___");
 		System.out.println("-\\hai sa comparam mere cu pere\\-");
-		
+		int numer = 0;
 		Scanner user_int = new Scanner(System.in);
 		System.out.print("provide your integer:");
-		int numar = user_int.nextInt();
+		
+		try {
+			int numar = user_int.nextInt();
+		}
+		catch (Exception e) {
+			System.out.print("value invalid");
+		}
+		
 		System.out.print("you have entered:" + numar);
 	
 	}

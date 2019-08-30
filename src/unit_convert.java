37be9c2a0dc5c1d1ab6bdfef31f31c4d1b90454c
dennis <dennis.bernic@gmail.com>
2019-08-31 00:03:40 +0300

    catch indefinitely

--- src/unit_convert.java
+++ src/unit_convert.java
@@ -5,18 +5,24 @@ public class unit_convert {
 	public static void main (String[] args) {
 		System.out.println("___the conversion is being started___");
 		System.out.println("-\\hai sa comparam mere cu pere\\-");
-		int numer = 0;
+
 		Scanner user_int = new Scanner(System.in);
-		System.out.print("provide your integer:");
 		
-		try {
-			int numar = user_int.nextInt();
+		boolean varType = true ;
+		while(varType) {
+			try {
+				System.out.print("provide your integer:");
+				int numar = user_int.nextInt();
+				System.out.print("you have entered:" + numar);
+				varType = false;
+			}
+			catch (Exception e) {
+				System.out.print("value invalid");
+			}
 		}
-		catch (Exception e) {
-			System.out.print("value invalid");
-		}
+
 		
-		System.out.print("you have entered:" + numar);
+		
 	
 	}
 

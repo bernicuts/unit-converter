5d8c2a099ee8d76df122b620d5e0bc9c0f69e657
bernicuts <dennis.frtlc@gmail.com>
2019-09-14 00:56:34 +0300

    adfa

---
+++ src/unitConverterFunctions.java
@@ -0,0 +1,14 @@
+
+public class unitConverterFunctions {
+
+	private static void isLegal() {
+		System.out.print("function works");
+	}
+
+
+	private static int getUserInt(int Number) {
+		if (True)
+		return this.Number;
+	}
+	
+}
--- src/unit_convert.java
+++ src/unit_convert.java
@@ -1,42 +1,46 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-
 public class unit_convert {
 
 	public static void main (String[] args) {
 		System.out.println("Unit converter has started (v1.0)");
-		
+		getUserInt();
+		//isLegal("22");
+	}
+
+	private static void getUserInt() {	
 		boolean isCorrectlyProvisioned = false ;
 		Scanner consoleReader = null;
 		
 		while(!isCorrectlyProvisioned) {
 			System.out.print("Enter a valid integer:");
+			consoleReader = new Scanner(System.in);
+			String enteredNumber = consoleReader.nextLine();
+			
 			try {
-				consoleReader = new Scanner(System.in);
-				int enteredNumber = consoleReader.nextInt();
-				isCorrectlyProvisioned = true;
-				System.out.print("You have successfully entered: " + enteredNumber);
 				
-				private boolean isLegal(int enteredNumber1) {
-					boolean a = false;
-					if (enteredNumber1 > 0);
-						a = True;
-					return a;
-				}
-
-				private int getUserInt(){
-					if (isLegal(Number))
-					return this.Number;
-				}
-				
-				
-				
-			} catch (InputMismatchException ex) {
+				isCorrectlyProvisioned = isLegal(enteredNumber);
+				System.out.print("You have successfully entered: " + enteredNumber);			
+			} 
+			catch (NumberFormatException ex) {
 				System.out.println("Please enter a valid number...\n");
 			}
 		}
 		
 		 consoleReader.close();
 	}
+	
+	private static boolean isLegal(String str) throws NumberFormatException {
+		System.out.print("\n checking value validity:\n");
+		System.out.print(Integer.parseInt( str ));
+		if (Integer.parseInt( str ) > 0) {
+			System.out.print("\n your value is valid\n");
+			return true;
+		}
+			return false;
+	}
+		
 }
+
+	

c2e2ef912ded2dbd572ac5331055d53da86d62bd
bernicuts <dennis.frtlc@gmail.com>
2019-09-14 22:15:13 +0300

    task2done

--- src/unit_convert.java
+++ src/unit_convert.java
@@ -5,42 +5,35 @@ public class unit_convert {
 
 	public static void main (String[] args) {
 		System.out.println("Unit converter has started (v1.0)");
-		getUserInt();
-		//isLegal("22");
+		
+		int number = getUserInt();
+		System.out.println("Your Value is:" + number);		
 	}
 
-	private static void getUserInt() {	
+	private static int getUserInt() {	
 		boolean isCorrectlyProvisioned = false ;
 		Scanner consoleReader = null;
+		int enteredNumber = 0;
 		
 		while(!isCorrectlyProvisioned) {
 			System.out.print("Enter a valid integer:");
 			consoleReader = new Scanner(System.in);
-			String enteredNumber = consoleReader.nextLine();
-			
+						
 			try {
-				
-				isCorrectlyProvisioned = isLegal(enteredNumber);
-				System.out.print("You have successfully entered: " + enteredNumber);			
+				enteredNumber = consoleReader.nextInt();
+				isCorrectlyProvisioned = valueIsLegal(enteredNumber);					
 			} 
-			catch (NumberFormatException ex) {
+			
+			catch (InputMismatchException ex) {
 				System.out.println("Please enter a valid number...\n");
 			}
 		}
-		
 		 consoleReader.close();
+		 return enteredNumber;
 	}
 	
-	private static boolean isLegal(String str) throws NumberFormatException {
+	private static boolean valueIsLegal(int value) {
 		System.out.print("\n checking value validity:\n");
-		System.out.print(Integer.parseInt( str ));
-		if (Integer.parseInt( str ) > 0) {
-			System.out.print("\n your value is valid\n");
-			return true;
-		}
-			return false;
+		return value > 0;			
 	}
-		
-}
-
-	
+}	
